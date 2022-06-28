3aac6d92476909f9b7ffc202f4a0e99387048b95
Szymon Skorupka <szymon.skorupka757@gmail.com>
2022-06-28 19:33:04 +0200

    proba dodania kontrolera

---
+++ poprawaAPBD/Controllers/TeamController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace poprawaAPBD.Controllers
+{
+    public class TeamController : Controller
+    {
+
+        
+
+        //[HttpGet]
+        //public async Task<IActionResult>  (int TeamID)
+        //{
+
+        //}
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
